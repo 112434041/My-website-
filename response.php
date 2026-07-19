f68e53eabab3858797e703c548a602debab582d1
112434041 <112434041@kanchiuniv.ac.in>
2026-07-19 22:38:53 +0530

    Add files via upload

---
+++ response.php
@@ -0,0 +1,23 @@
+<?php
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+// Valid credentials
+$validUser = "admin";
+$validPass = "12345";
+
+echo "<h2>PHP Response</h2>";
+
+if ($username == $validUser && $password == $validPass)
+{
+    echo "<h3 style='color:green;'>Login Successful!</h3>";
+    echo "Welcome, " . htmlspecialchars($username);
+}
+else
+{
+    echo "<h3 style='color:red;'>Login Failed!</h3>";
+    echo "Invalid Username or Password.";
+}
+
+?>
